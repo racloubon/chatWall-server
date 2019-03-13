e159de2f924e9ee46b7ff07649cb7967da5e5432
Egill Hreinsson <egillh210@gmail.com>
2019-03-13 16:07:45 +0100

    refactor: login controller

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -16,11 +16,11 @@ module.exports.signIn = async (ctx, next, User = models.User) => {
   const userColonPassword = atob(authHeader.split('Basic ')[1]);
   const [username, password] = userColonPassword.split(':');
 
-  const jwt_token = jwt.sign({username}, jwt_secret);
+  const jwt_token = jwt.sign({ username }, jwt_secret);
 
   try {
-    const usern = await User.findOne({where: {username}});
-    const match = await bcrypt.compare(password, user.password);
+    const user = await User.findOne({ where: { username } });
+    await bcrypt.compare(password, user.password);
     ctx.body = { jwt_token, username };
     ctx.status = 200;
   }
@@ -56,83 +56,3 @@ module.exports.create = async (ctx, next, User = models.User) => {
   }
 
 };
-
-
-
-
-
-
-
-// const bcrypt = require('bcrypt');
-// const atob = require('atob');
-// const jwt = require('jsonwebtoken');
-// const models = require('../models');
-// require('dotenv').config({path:__dirname+'/./../../.env'});
-//
-// const jwt_secret = process.env.JWT_SECRET;
-//
-// module.exports.signIn = async (ctx, next) => {
-//   if ('GET' != ctx.method) return await next();
-//
-//   const authHeader = ctx.headers.authorization;
-//   if (!authHeader) throw new Error('No authorization header');
-//
-//   const userColonPassword = atob(authHeader.split('Basic ')[1]);
-//   const [username, password] = userColonPassword.split(':');
-//
-//   if (!username || !password) throw new Error('Bad Credentials');
-//
-//   const user = await models.User.findOne({where: {username: username}});
-//   if (!user) throw new Error('Bad user or password');
-//   const match = await bcrypt.compare(password, user.password);
-//
-//   if (!user || !match) {
-//     ctx.status = 401;
-//     return;
-//   } else {
-//     ctx.status = 200;
-//     const token = jwt.sign({username: username}, jwt_secret);
-//     ctx.body = {
-//       jwt_token: token,
-//       username
-//     };
-//   }
-// };
-//
-// module.exports.create = async (ctx, next) => {
-//   if ('POST' != ctx.method) return await next();
-//
-//   const userData = ctx.request.body;
-//
-//   let user = await models.User.findOne({where: {username: userData.username}});
-//
-//   if (user) {
-//     ctx.status = 400;
-//     ctx.body = {
-//       errors: [
-//         'Username already exists.'
-//       ]
-//     };
-//   } else {
-//     const encryptedPassword = await bcrypt.hash(userData.password, 10);
-//
-//     user = {
-//       username: userData.username,
-//       password: encryptedPassword,
-//       email: userData.email
-//     };
-//     const createdUser = await models.User.create(user);
-//     const token = jwt.sign({username: user.username}, jwt_secret);
-//     ctx.body = {
-//       username: user.username,
-//       jwt_token: token
-//     };
-//     ctx.status = 201;
-//   }
-// };
-//
-// module.exports.getAll = async (ctx, next) => {
-//   if ('GET' != ctx.method) return await next();
-//
-//   ctx.body = await models.User.findAll();
-// };
