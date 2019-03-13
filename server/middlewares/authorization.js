77f4d6173312bec8ac7191b41c39c1596f3078de
Egill Hreinsson <egillh210@gmail.com>
2019-03-13 16:08:38 +0100

    refactor: autorization controller and add jwt validation

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,32 +1,26 @@
 const jwt = require('jsonwebtoken');
+const validate = require('validator');
 require('dotenv').config({path:__dirname+'/./../../.env'});
 
 const jwt_secret = process.env.JWT_SECRET;
 
 const authorize = async (ctx, next) => {
-  const authHeader = ctx.headers.authorization;
+  const { authorization: authHeader } = ctx.headers;
 
-  if (!authHeader) {
-    ctx.status = 401;
-    ctx.body = 'There is no authorization header';
-    return;
-  }
+  const jwt_token = authHeader.substring(7);
 
-  const jwt_token = authHeader.split(' ')[1];
-  let decoded;
-
+  if (!validate.isJWT(jwt_token)) throw Error('Auth header not a valid jwt');
 
   try {
-    decoded = jwt.verify(jwt_token, jwt_secret);
-  } catch (err) {
-    ctx.status =401;
+    ctx.user = { username } = jwt.verify(jwt_token, jwt_secret)
+    await next();
+  }
+  catch (err) {
+    ctx.status = 401;
     ctx.body = err.message;
-    return;
   }
 
-  ctx.user = {username: decoded.username};
-
-  await next();
 };
+
 
 module.exports = authorize;
