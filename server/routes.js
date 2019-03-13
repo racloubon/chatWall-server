507bb4a0a13813264f9dd88430432a93fde9a633
Egill Hreinsson <egillh210@gmail.com>
2019-03-13 17:10:20 +0100

    removed channels validation middleware from routing

--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,7 @@ const UsersController = require('./controllers/users.controller');
 const ChannelsController = require('./controllers/channels.controller');
 const MessagesController = require('./controllers/messages.controller');
 
-const channelRequestValidation = require('./middlewares/channelsValidation');
+
 const messagesValidation = require('./middlewares/messagesValidation');
 
 const router = require('koa-router')();
@@ -17,7 +17,7 @@ router.get('/', (ctx, next) => {
 router.post('/users', UsersController.create);
 router.get('/sign-in', UsersController.signIn);
 
-router.post('/channels', authMiddleware, channelRequestValidation, ChannelsController.create);
+router.post('/channels', authMiddleware, ChannelsController.create);
 router.post('/messages', authMiddleware, messagesValidation, MessagesController.create);
 router.get('/messages', authMiddleware, MessagesController.getMessages);
 router.put('/messages', authMiddleware, MessagesController.voteMessage);
