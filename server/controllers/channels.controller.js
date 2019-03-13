68c6310f01610065418649b8a22823461324b56a
Egill Hreinsson <egillh210@gmail.com>
2019-03-13 17:08:55 +0100

    added channel name validation in controller

--- server/controllers/channels.controller.js
+++ server/controllers/channels.controller.js
@@ -1,10 +1,14 @@
 const models = require('../models');
 const ids = require('short-id');
+const validate = require('validator');
 
 module.exports.create = async (ctx, next, Channel = models.Channel) => {
 
   const { channel: name } = ctx.request.body;
   const { username: creator } = ctx.user;
+
+  if (!validate.isLength(name, { min: 5, max: 100 })) throw Error('Channel too short/long. Min: 5 Max: 100');
+
   const pin = ids.generate();
 
   try {
